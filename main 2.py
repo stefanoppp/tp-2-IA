09cf27494c5d27b39319720a517473f5a2a39b8a
Stefano <49915584+stefanoppp@users.noreply.github.com>
2022-09-30 12:08:06 -0300

    Add files via upload

--- main 2.py
+++ main 2.py
@@ -6,82 +6,37 @@ class Perceptron:
         self.pesos=pesos
         self.entradas=entradas
         self.salidas=salidas
-        self.lr=0.1
-
-    def balance(self):
-        salidas=self.obtener_salidas()
-        print(salidas)
-
-    def obtener_salidas(self):
-        iteraciones=0
-        i=0
-        fallos=4
-        while True:
-            errores=[]
-            for entrada in self.entradas:
-                
-                deltas_peso=[]
-                producto_escalar=np.dot(entrada,self.pesos)
-                salida_obtenida=self.sigmoidea(producto_escalar)
+        self.lr=0.4
+        self.tolerancia=4
+        self.actualizaciones=0
+    
+    def actualizar(self):
+            i=0
+            while i in range(len(self.entradas)):        
+                prod_escalar=np.dot(self.pesos,self.entradas[i])
+                salida_obtenida=self.sigmoidea(prod_escalar)
                 error=self.salidas[i]-salida_obtenida
-                delta=salida_obtenida*(1-salida_obtenida)*error
-                errores.append(error)
-                for value in entrada:
-                    delta_peso=self.lr*value*delta
-                    deltas_peso.append(delta_peso)
-                    
-                self.pesos+=deltas_peso
-                if abs(error)<=0.15:
-                    fallos=fallos-1
-                    if fallos==0:
-                        print(self.pesos)
-                        print("cantidad de iteraciones: ", iteraciones)
-                        break
+                if abs(error)<0.01:
+                    self.tolerancia-=1
+                    i+=1
+                    if self.tolerancia==0:
+                        print("Nuevos pesos: ",self.pesos)
+                        print("Actualizaciones: ",self.actualizaciones)
+        
                 else:
-                    fallos=4  
-                iteraciones+=1
-        # print("nuevos pesos")
-        # print(self.pesos)
-
-                                              
-    def sigmoidea(self,salida_obtenida):
-        sig = 1/(1 + math.exp(-salida_obtenida))
+                    for j in range(len(self.entradas[i])):
+                        self.pesos[j]=self.pesos[j]+(self.lr*error*self.entradas[i][j])
+                    self.actualizaciones+=1
+                    self.tolerancia=4
+                    i=0
+                    
+    def sigmoidea(self,prod_escalar):
+        sig = 1 / (1 + math.exp(-prod_escalar))
         return sig
-
-np_matrix_pesos=np.array([0.9,0.66,-0.2])
-np_matrix_entradas=np.array([[1,0,1],[1,1,1],[0,1,0],[0,1,1]])
-np_matrix_salidas=np.array([0,1,0,0])
+        
+np_matrix_pesos=np.array([0.6473185,0.37817776,0.33160055]) 
+np_matrix_entradas=np.array([[1,1,1],[1,1,0],[1,0,1],[1,0,0]]) 
+np_matrix_salidas=np.array([1,0,0,0]) 
 and_perceptor=Perceptron(np_matrix_pesos,np_matrix_entradas,np_matrix_salidas)
 if __name__=="__main__":
-    
-    print(and_perceptor.balance())
-    
-    # contador=0
-    # pesos=[]
-    # peso1=[]
-    # peso2=[]
-    # peso3=[]
-    # while contador<100:
-    #     print(and_perceptor.balanceo_peso())
-    #     # pesos_balanceados=and_perceptor.balanceo_peso()
-    #     # pesos.append(pesos_balanceados)
-    #     contador+=1
-        
-        
-    # for peso in pesos:
-    #     for j in peso:
-    #         count=0
-    #         for k in j:
-    #             if count==0:
-    #                 peso1.append(k)
-    #             if count==1:
-    #                 peso2.append(k)
-    #             if count==2:
-    #                 peso3.append(k)
-    #             count+=1 
-    # print(peso1)
-    # print(peso2) 
-    # plt.plot(peso1,'g')
-    # plt.plot(peso2,'r')
-    # plt.plot(peso3,'k')
-    # plt.show()
+    and_perceptor.actualizar()
